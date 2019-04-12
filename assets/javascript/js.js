f1764357afd32548dd6e3af1935aa7e501b9d983
Non Dewman <Dnewman1934@yahoo.com>
2019-04-11 19:48:22 -0500

    game logic lil java

---
+++ assets/javascript/js.js
@@ -0,0 +1,27 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCw1iDS84Bz7Wk5ifElmdhN1fyQ4LsRALY",
+    authDomain: "pokerdata-23592.firebaseapp.com",
+    databaseURL: "https://pokerdata-23592.firebaseio.com",
+    projectId: "pokerdata-23592",
+    storageBucket: "pokerdata-23592.appspot.com",
+    messagingSenderId: "144592076912"
+};
+firebase.initializeApp(config);
+const DB = firebase.database();
+const connectionRef = DB.ref("/players")
+const connectedRef = DB.ref(".info/connected")
+const gameState = DB.ref("gameState");
+
+connectedRef.on("value", snapshot => {
+    if (snapshot.val()) {
+        const connection = connectionRef.push(true);
+        game.localID = connection.key;
+        connection.onDisconnect().remove(() => {
+            gameState.remove();
+        });
+    }
+});
+
+//Unsplash ajax call
+//
