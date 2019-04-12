a232cdba97a649b3f123255e06486876a571d8a1
Non Dewman <Dnewman1934@yahoo.com>
2019-04-11 21:14:23 -0500

    card deck array

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,4 +1,5 @@
 // Initialize Firebase
+const cardDeck = ['2h', '2s', '2d', '2c', '3h', '3s', '3d', '3c', '4h', '4s', '4d', '4c', '5h', '5s', '5d', '5c', '6h', '6s', '6d', '6c', '7h', '7s', '7d', '7c', '8h', '8s', '8d', '8c', '9h', '9s', '9d', '9c', '10h', '10s', '10d', '10c', 'jh', 'js', 'jd', 'jc', 'qh', 'qs', 'qd', 'qc', 'kh', 'ks', 'kd', 'kc', 'ah', 'as', 'ad', 'ac',]
 var config = {
     apiKey: "AIzaSyCw1iDS84Bz7Wk5ifElmdhN1fyQ4LsRALY",
     authDomain: "pokerdata-23592.firebaseapp.com",
@@ -22,7 +23,7 @@ connectedRef.on("value", snapshot => { //assign user IDs
         });
     }
 });
-const pagereturn = 1
+const pagereturn = 1 //ajax unsplash query for card backings
 let query = 'dogs'
 const QueryUrl = `https://api.unsplash.com/search/photos?page=${pagereturn}&query=${query}&client_id=1a8efd59c4b5cb5e177aea595dc217c32b578bb3d681940bb9c01a4bf5cc0919`;
 $.ajax({
@@ -32,3 +33,12 @@ $.ajax({
     var callBack = response.data;
     console.log(response);
 });
+
+//deal card function
+playerHand = []
+
+//bet/check function to run as long as cardsbeingDealt === true + 1?
+
+//function to compare hands and select victor
+
+
