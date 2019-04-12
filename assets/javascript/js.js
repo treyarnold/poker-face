99fb914eeb22bfa198c367fbabec5aaf60b70e73
Non Dewman <Dnewman1934@yahoo.com>
2019-04-11 21:15:15 -0500

    deck array

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,6 +1,7 @@
 // Initialize Firebase
 const cardDeck = ['2h', '2s', '2d', '2c', '3h', '3s', '3d', '3c', '4h', '4s', '4d', '4c', '5h', '5s', '5d', '5c', '6h', '6s', '6d', '6c', '7h', '7s', '7d', '7c', '8h', '8s', '8d', '8c', '9h', '9s', '9d', '9c', '10h', '10s', '10d', '10c', 'jh', 'js', 'jd', 'jc', 'qh', 'qs', 'qd', 'qc', 'kh', 'ks', 'kd', 'kc', 'ah', 'as', 'ad', 'ac',]
-var config = {
+let shuffledDeck = [];
+const config = {
     apiKey: "AIzaSyCw1iDS84Bz7Wk5ifElmdhN1fyQ4LsRALY",
     authDomain: "pokerdata-23592.firebaseapp.com",
     databaseURL: "https://pokerdata-23592.firebaseio.com",
