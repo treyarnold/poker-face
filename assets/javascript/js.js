a6026205e9be0b9ecb1f5664ebe2e72f83bceb5d
Non Dewman <Dnewman1934@yahoo.com>
2019-04-13 10:56:58 -0500

    full authentication (:

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -279,7 +279,101 @@ function initApp2() {
 window.onload = function () {
     initApp2();
 };
-
+function checkLoginState(event) {
+    if (event.authResponse) {
+        // User is signed-in Facebook.
+        var unsubscribe = firebase.auth().onAuthStateChanged(function (firebaseUser) {
+            unsubscribe();
+            // Check if we are already signed-in Firebase with the correct user.
+            if (!isUserEqual(event.authResponse, firebaseUser)) {
+                // Build Firebase credential with the Facebook auth token.
+                // [START facebookcredential]
+                var credential = firebase.auth.FacebookAuthProvider.credential(
+                    event.authResponse.accessToken);
+                // [END facebookcredential]
+                // Sign in with the credential from the Facebook user.
+                // [START authwithcred]
+                firebase.auth().signInAndRetrieveDataWithCredential(credential).catch(function (error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // The email of the user's account used.
+                    var email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    var credential = error.credential;
+                    // [START_EXCLUDE]
+                    if (errorCode === 'auth/account-exists-with-different-credential') {
+                        alert('You have already signed up with a different auth provider for that email.');
+                        // If you are using multiple auth providers on your app you should handle linking
+                        // the user's accounts here.
+                    } else {
+                        console.error(error);
+                    }
+                    // [END_EXCLUDE]
+                });
+                // [END authwithcred]
+            } else {
+                // User is already signed-in Firebase with the correct user.
+            }
+        });
+    } else {
+        // User is signed-out of Facebook.
+        // [START signout]
+        firebase.auth().signOut();
+        // [END signout]
+    }
+}
+// [END facebookcallback]
+/**
+ * Check that the given Facebook user is equals to the  given Firebase user
+ */
+// [START checksameuser]
+function isUserEqual(facebookAuthResponse, firebaseUser) {
+    if (firebaseUser) {
+        var providerData = firebaseUser.providerData;
+        for (var i = 0; i < providerData.length; i++) {
+            if (providerData[i].providerId === firebase.auth.FacebookAuthProvider.PROVIDER_ID &&
+                providerData[i].uid === facebookAuthResponse.userID) {
+                // We don't need to re-auth the Firebase connection.
+                return true;
+            }
+        }
+    }
+    return false;
+}
+// [END checksameuser]
+/**
+ * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+ *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+ *    out, and that is where we update the UI.
+ */
+function initApp() {
+    // Listening for auth state changes.
+    // [START authstatelistener]
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+            // [END_EXCLUDE]
+        } else {
+            // User is signed out.
+            // [START_EXCLUDE]
+            document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
+            document.getElementById('quickstart-account-details').textContent = 'null';
+            // [END_EXCLUDE]
+        }
+    });
+    // [END authstatelistener]
+}
 function game() {            //the whole game box, functions first then all the logic yeah?   
     const playerHand = ['']
     const smallBlind = 2;
@@ -334,8 +428,5 @@ function game() {            //the whole game box, functions first then all the 
         //something to rotate the blinds and dealer (class? ooooo that sounds, like a player.removeClass(smallBlind) player.addClass(bigBlind) or u no)
     }
     function newHand() { //a function to set the used deck back to full array and begin the deal function
-
-
     }
 }
-
