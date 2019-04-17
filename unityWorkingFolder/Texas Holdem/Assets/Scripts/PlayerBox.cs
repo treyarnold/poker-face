1d34ee3873f5256e41dcb3b1aed843a571622e0c
treyarnold <arnold.trey80@gmail.com>
2019-04-16 20:30:25 -0500

    changed branch after breaking trey branch

---
+++ unityWorkingFolder/Texas Holdem/Assets/Scripts/PlayerBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerBox : MonoBehaviour
+{
+  [SerializeField] TextMeshProUGUI playerName;
+  [SerializeField] TextMeshProUGUI playerFunds;
+  [SerializeField] TextMeshProUGUI currentBet;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    playerName.text = "Trey";
+    playerFunds.text = "250k";
+    currentBet.text = "10k";
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    
+  }
+}
