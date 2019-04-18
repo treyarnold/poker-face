ea6442ce6db20f6b24b9269fa510135c3f69570c
Trey Arnold <arnold.trey80@gmail.com>
2019-04-18 15:51:44 -0500

    Created sprites and now able to change cards in table in code

--- unityWorkingFolder/Texas Holdem/Assets/Scripts/GameSession.cs
+++ unityWorkingFolder/Texas Holdem/Assets/Scripts/GameSession.cs
@@ -1,34 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSession : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  [SerializeField] Image Flop1Image;
+  [SerializeField] Image Flop2Image;
+  [SerializeField] Image Flop3Image;
+  [SerializeField] Image TurnImage;
+  [SerializeField] Image RiverImage;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  int numCardsDealt;
+  int numCommunityCardsDealt;
 
-    public void CallButtonPressed()
-    {
-      Debug.Log("Call");
-    }
-    
-    public void FoldButtonPressed()
-    {
-      Debug.Log("Fold");
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    numCardsDealt = 0;    
+    numCommunityCardsDealt = 0;    
+  }
 
-    public void RaiseButtonPressed()
+  // Update is called once per frame
+  void Update()
+  {
+      
+  }
+
+  public void CallButtonPressed()
+  {
+    Debug.Log("Call");
+  }
+  
+  public void FoldButtonPressed()
+  {
+    Debug.Log("Fold");
+  }
+
+  public void RaiseButtonPressed()
+  {
+    Debug.Log("Raise");
+  }
+
+  public void DealButtonPressed()
+  {
+    switch (numCommunityCardsDealt)
     {
-      Debug.Log("Raise");
+      case 0:
+        DealFlop();
+        break;
+      case 3:
+        DealTurn();
+        break;
+      case 4:
+        DealRiver();
+        break;
     }
+  }
+
+  private void DealFlop()
+  {
+    Flop1Image.sprite = Resources.Load<Sprite>("Sprites/Cards/4h");
+    Flop2Image.sprite = Resources.Load<Sprite>("Sprites/Cards/3h");
+    Flop3Image.sprite = Resources.Load<Sprite>("Sprites/Cards/2h");
+    numCommunityCardsDealt = 3;
+  }
+
+  private void DealTurn()
+  {
+    TurnImage.sprite = Resources.Load<Sprite>("Sprites/Cards/5h");
+    numCommunityCardsDealt++;
+  }
+
+  private void DealRiver()
+  {
+    RiverImage.sprite = Resources.Load<Sprite>("Sprites/Cards/6h");
+    numCommunityCardsDealt++;
+  }
 
 }
--- unityWorkingFolder/Texas Holdem/Assets/Scripts/PlayerCards.cs
+++ unityWorkingFolder/Texas Holdem/Assets/Scripts/PlayerCards.cs
@@ -3,27 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerImage : MonoBehaviour
+public class PlayerCards : MonoBehaviour
 {
-  [SerializeField] Sprite playerAvatar;
 
-  Image avatarImage;
+  Image cardImage;
 
   // Start is called before the first frame update
   void Start()
   {
-    avatarImage = GetComponent<Image>();   
-    avatarImage.sprite = playerAvatar;
+    cardImage = GetComponent<Image>();  
   }
 
   // Update is called once per frame
   void Update()
   {
-
-  }
-
-  public void DealButtonPressed()
-  {
-    
+      
   }
 }
