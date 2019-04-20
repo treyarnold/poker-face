2d0b78b2a271235ad6871be5d76d32a33d0ee33a
Trey Arnold <arnold.trey80@gmail.com>
2019-04-19 22:17:20 -0500

    refactored scripts to remove errors. Created player boxes log their player number, so they know where they are

---
+++ unityWorkingFolder/Texas Holdem/Assets/Scripts/ActivePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log(transform.parent.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- unityWorkingFolder/Texas Holdem/Assets/Scripts/PlayerBox.cs
+++ unityWorkingFolder/Texas Holdem/Assets/Scripts/PlayerBox.cs
@@ -6,41 +6,14 @@ using TMPro;
 
 public class PlayerBox : MonoBehaviour
 {
-  [SerializeField] TextMeshProUGUI playerName;
-  [SerializeField] TextMeshProUGUI playerFunds;
-  [SerializeField] TextMeshProUGUI currentBet;
   [SerializeField] GameObject emptyChair;
-  [SerializeField] GameObject activePlayBox;
 
   GameObject chair;
-  GameObject activePlayerBox;
-  Vector2 originalPosition;
 
   // Start is called before the first frame update
   public void Start()
   {
-    originalPosition = gameObject.transform.position;
     chair = Instantiate(emptyChair) as GameObject;
     chair.transform.SetParent(gameObject.transform, false);
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    
-  }
-
-  public void SitHere()
-  {
-    // Script myParent = transform.parent.GetComponent<parentscriptclass>();
-    // Destroy(chair);
-    activePlayerBox = Instantiate(activePlayBox) as GameObject;
-    GameObject grandParent = transform.parent.parent.gameObject;
-    activePlayerBox.transform.SetParent(grandParent.transform, false);
-    GameObject newActive = grandParent.transform.GetChild (1).gameObject;
-    Debug.Log(newActive.name);
-    newActive.transform.position = originalPosition;
-    Destroy(transform.parent.gameObject);
-    Debug.Log(transform.parent.parent.name);
   }
 }
